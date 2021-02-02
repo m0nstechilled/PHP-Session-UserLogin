b1332d5121880e96633f426b0befd7236036aaa3
velip <anders_teehee@hotmail.no>
2021-02-02 23:47:11 +0100

    Update database.php

--- classes/database.php
+++ classes/database.php
@@ -5,9 +5,9 @@ class database {
 	public function __construct() {
 
 		return new PDO(
-		    'mysql:host=localhost;dbname=trundle',
-		    "root",
-		    "worktroll911",
+		    'mysql:host=localhost;dbname=DATABASE',
+		    "USERNAME",
+		    "PASSWORD",
 		    array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
 
 	}
