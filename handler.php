02f646003adea4b5e8635f1c8da68166fe99ac46
velip <anders_teehee@hotmail.no>
2021-02-02 23:46:35 +0100

    Update handler.php

--- handler.php
+++ handler.php
@@ -2,7 +2,7 @@
 
 try {
 
-   $handler = new PDO('mysql:host=localhost;dbname=test','root','sonnie',
+   $handler = new PDO('mysql:host=localhost;dbname=DATABASE','USERNAME','PASSWORD',
       array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
    $handler->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
